175ccf5076115334f963fff533237bee3d5c8ff2
super-time1 <168475235+super-time1@users.noreply.github.com>
2024-08-09 23:38:42 -0400

    update

---
+++ src/Components/WorkReducer.jsx
@@ -0,0 +1,9 @@
+export default function WorkReducer(state, dispatch) {
+    switch(type) {
+        case "edit_task": {
+            if (title === "") {
+                return state;
+            }
+        }
+    }
+}
